bc7d6e23288413aa8936cb1eaf2e5b5fcdf22b34
Jesse-ox <jessehunt10@gmail.com>
2021-12-20 17:57:58 -0500

    Wp update theme

--- wordpress/wp-content/themes/portfolio2021/index.php
+++ wordpress/wp-content/themes/portfolio2021/index.php
@@ -3,25 +3,11 @@
 
 <?php 
 	if ( is_page("home") ) {
-		echo "<h1>Home</h1>";
+		include ('templates/components/home.php');
 	}
 
 	if ( is_page("list") ) {
-		$args = array(  
-	  		'post_type' => 'projects',
-		);
-
-		$loop = new WP_Query( $args ); 
-	  
-		while ( $loop->have_posts() ) : $loop->the_post(); 
-	 		include("templates/components/project-card.php");
-		endwhile;
-
-		wp_reset_postdata(); 
-	}
-
-	if ( is_singular("projects") ) {
-		echo "<h2>" . the_field("name") . "</h2>";
+		echo "list";
 	}
 
 ?>
