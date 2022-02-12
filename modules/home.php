0b71e8b3a6160a75a33af7682b61c7e9776862af
Jesse-ox <jessehunt10@gmail.com>
2022-02-11 22:52:06 -0500

    add projects teaser

--- modules/home.php
+++ modules/home.php
@@ -1,22 +1,24 @@
 
-<section class='welcome'>
+<section class='home'>
 	<inner-column>
-		<landing>
 
-			<statement>
+		<div class="landing">
+			<header>
 				<h2 class="mild-voice">WELCOME,</h2>
 
 				<h1 class="loud-voice">I'm Jesse and I am looking for a <span>full-time position</span> with a creative and standout team!</h1>
-			</statement>
+			</header>
 
-			<mission>
+			<text-content>
 				<p class="calm-voice">I am a<span>Front-End Web Developer</span>and<span>Designer</span>.</p>
 
 				<p class="calm-voice">I have an affinity for building things on the web. My purpose for this website is to showcase what I have achieved through my projects.</p>
 
 				<p class="calm-voice">Indulge. If you want to chat or have any questions<a class="contact" href="#">contact me!</a></p>
-			</mission>
-			
-		</landing>
+			</text-content>
+		</div>
+		
 	</inner-column>
 </section>
+
+<?php include('modules/templates/projects-teaser.php'); ?>
---
+++ modules/templates/projects-teaser.php
@@ -0,0 +1,33 @@
+
+<section class="off-the-press">
+	<inner-column>
+		
+		<header>
+			<h3 class="">PROJECTS</h3>
+
+			<h1 class="">Latest Works</h1>
+		</header>
+		
+		<text-content>
+			<div class="">
+				<picture>
+					<img src="images/responsive-layouts.svg">
+				</picture>
+
+				<h2 class="">DAILYS</h2>
+				<p class="">A responsive layout collection from modules and sections taken from websites in the wild. They are either responsive by nature or made responsive.</p>
+			</div>
+
+			<div class="">
+				<picture>
+					<img src="images/responsive-layouts.svg">
+				</picture>
+
+				<h2 class="">DAILYS</h2>
+				<p class="">A responsive layout collection from modules and sections taken from websites in the wild. They are either responsive by nature or made responsive.</p>
+			</div>
+			
+		</text-content>
+
+	</inner-column>
+</section>
