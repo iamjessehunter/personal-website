5fe9918b0bc533ee3b6a0d4352a073727acb776a
Jesse-ox <jessehunt10@gmail.com>
2022-03-04 13:24:50 -0500

    solidworks section update

--- projects/responsive-layouts/modules/solidworks.php
+++ projects/responsive-layouts/modules/solidworks.php
@@ -1,18 +1,17 @@
 
 
-<section class="layout">
-	<inner-column>
-		
-	<section class="layout-info">
-		<h3 class="mild-voice">
-			Section Layout:
-			<a href="https://solidworks.com/" target="_blank">Solidworks</a>
+<section class="solidworks">
+	<div class="layout-info">
+		<h3 class="mild-voice">Section Layout:
+			<a href="https://www.solidworks.com/" target="_blank">Solidworks</a>
 		</h3>
-	</section>
+	</div>
 
-	<section class="solidworks">
-		<inner-column>
 			
+	
+	<section class="solid-wrapper">
+		<inner-column>
+			<!-- Grid Content -->
 			<grid-wrapper>
 				<div class="mod mod-1"></div>
 
@@ -20,9 +19,9 @@
 
 				<div class="mod mod-3">
 					<div class="mod-3-text">
-						<h3 class="slow-voice">What's New in SOLIDWORKS 2022</h3>
-						<p class="link-voice">New Enchancements to streamline and accelerate your entire product development process which will improve efficiency, accuracy, and collaboration.</p>
-						<a href="#" class="link-voice">Learn More</a>
+						<h2 class="loud-voice">Grid Layout inspired by Solidworks</h2>
+						<p class="mild-voice">This grid layout used by solidworks caught my attention immediately. It was my first experience using grid three months in as a web developer.</p>
+						<a href="#" class="quiet-voice">> I'm a CTA link</a>
 					</div>
 				</div>
 
@@ -32,9 +31,9 @@
 
 				<div class="mod mod-6">
 					<div class="mod-6-text">
-						<h3 class="slow-voice">DELMIAWorks Manufacturing ERP</h3>
-						<p class="link-voice">An end-to-end manufacturing ERP System with a “shop floor first” focus that enables mid-market manufacturers to streamline processes across their supply chain and achieve operational excellence.</p>
-						<a href="#" class="link-voice">> Learn More</a>
+						<h2 class="loud-voice">Try Resizing the Browser</h2>
+						<p class="mild-voice">...or whichever device you are viewing this on the layout will change.</p>
+						<a href="#" class="quiet-voice">> I'm a CTA link</a>
 					</div>
 				</div>
 
@@ -44,13 +43,13 @@
 
 				<div class="mod mod-9">
 					<div class="mod-9-text">
-						<h3 class="slow-voice">3DEXPERIENCE SOLIDWORKS® for Makers</h3>
-						<p class="link-voice">Start making with the world’s best design tools – now for hobbyists and personal projects.</p>
-						<a href="#" class="link-voice">> Learn More</a>
+						<h2 class="loud-voice">Heading Level Two</h2>
+						<p class="mild-voice">I am body copy that will preface information to get you to click this module or the link below me.</p>
+						<a href="#" class="quiet-voice">> Learn More</a>
 					</div>
 				</div>
-
 			</grid-wrapper>
+
+		</inner-column>
 	</section>
-	</inner-column>
 </section>
