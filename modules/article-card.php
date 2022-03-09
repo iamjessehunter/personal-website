14d489bdab31fa277f195defb31cb01194271ac5
Jesse-ox <jessehunt10@gmail.com>
2022-03-08 22:20:06 -0500

    add Flexbox and JS codepen links

--- modules/article-card.php
+++ modules/article-card.php
@@ -1,15 +1,24 @@
 
 <article-card>
 
-	<picture>
-		<img src="<?=$article["image"]?>">
-	</picture>
+	<a href="<?=$article["link"]?>" target="_blank">
+		<picture>
+			<img src="<?=$article["image"]?>">
+		</picture>
+	</a>
 
 	<text-content>
-		<a class='topic mild-voice' href="#"><?=$article["heading"]?></a>
+		<a class='topic mild-voice' 
+			href="<?=$article["link"]?>" 
+			target="_blank"><?=$article["heading"]?>
+		</a>
 
 		<p class='calm-voice'><?=$article["description"]?></p>
 
-		<a class="view link-voice" href="<?=$article["link"]?>" target="_blank"><?=$article["site"]?></a>
+		<a class="view link-voice" 
+			href="<?=$article["link"]?>" 
+			target="_blank"><?=$article["site"]?>
+		</a>
 	</text-content>
+	
 </article-card>
--- modules/templates/experiments-intro.php
+++ modules/templates/experiments-intro.php
@@ -11,19 +11,13 @@
 
 			<experiment-grid>
 				<experiment-card>
-					<a class="showcase mild-voice" href="#">Flexbox Yoga</a>
+					<a class="showcase mild-voice" href="https://codepen.io/collection/ZMBVyB" target="_blank">Flexbox Yoga</a>
 
 					<p class="calm-voice">Flexbox pens with divs and color.</p>
 				</experiment-card>
 
 				<experiment-card>
-					<a class="showcase mild-voice" href="#">Uncle Bill Murray</a>
-
-					<p class="calm-voice">Early exploration in my journey as a web developer. A simple site made for Bill Murray.</p>
-				</experiment-card>
-
-				<experiment-card>
-					<a class="showcase mild-voice" href="#">Javascript Fun</a>
+					<a class="showcase mild-voice" href="https://codepen.io/collection/RzoZey" target="_blank">Javascript Fun</a>
 
 					<p class="calm-voice">Color - Checkbox - Theme Switch</p>
 				</experiment-card>
@@ -32,6 +26,12 @@
 					<a class="showcase mild-voice" href="#">Headers Without Burgers</a>
 
 					<p class="calm-voice">Ditch the hamburger menu! Here are some examples of different header layouts you can try.</p>
+				</experiment-card>
+
+				<experiment-card>
+					<a class="showcase mild-voice" href="#">Uncle Bill Murray</a>
+
+					<p class="calm-voice">Early exploration in my journey as a web developer. A simple site made for Bill Murray.</p>
 				</experiment-card>
 			</experiment-grid>
 
